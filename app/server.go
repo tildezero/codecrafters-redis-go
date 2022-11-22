b34cf217382777391f7bc05238d1f2794c970841
suhas <hi@suhas.one>
2022-11-22 13:05:31 -0600

    PONG

--- app/server.go
+++ app/server.go
@@ -18,7 +18,8 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	lsr, err := l.Accept()
+	lsr.Write([]byte("+PONG\r\n"))
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
